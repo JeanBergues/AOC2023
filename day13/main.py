e356bd11754bb68b47267118885f8b52133ef464
JeanBergues <jandemandan@outlook.com>
2025-05-08 11:00:41 +0200

    Day 13 complete

--- day13/main.py
+++ day13/main.py
@@ -36,10 +36,52 @@ def find_potential_reflection_axes(p):
     return potential_axes
 
 
+def find_smudged_reflection_axes(p):
+    potential_axes = list(range(1, len(p[0])))
+    ax_dict = {x: 0 for x in potential_axes}
+
+    for row in p:
+        new_axes = dict()
+        for ax in ax_dict:
+            l, r = row[0:ax], row[ax:]
+            length = min(len(l), len(r))
+            cl, cr = l[(len(l) - length):], r[0:length]
+
+            diff = ax_dict[ax] + count_differences(cl, cr)
+            if diff <= 1:
+                new_axes[ax] = diff
+        ax_dict = new_axes
+    
+    return [a for a, v in ax_dict.items() if v == 1]
+
+
+def count_differences(cl, cr):
+    diff = 0
+    for x, y in zip(cl, reversed(cr)):
+        if x != y:
+            diff += 1
+    return diff
+
+
 def main_b(puzzle_input):
     total = 0
 
-    return 0
+    for pattern in puzzle_input.read().split('\n\n'):
+        p = pattern.splitlines()
+        # Check for horizontal reflections
+        potential_axes = find_smudged_reflection_axes(p)
+            
+        if potential_axes:
+            total += potential_axes[0]
+        else:
+            tp = []
+            for i in range(len(p[0])):
+                tp.append([row[i] for row in p])
+
+            potential_axes = find_smudged_reflection_axes(tp)
+            total += 100 * potential_axes[0]
+
+    return total
 
 
 if __name__ == '__main__':
